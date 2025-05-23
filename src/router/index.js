02f73e7edba6d784b16aa9b008260bf6711624a5
kenneth898 <kennethtan0601@gmail.com>
2025-05-23 17:55:38 +0800

    remove popup & add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,23 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'; // Example component
-
+import NotFound from '@/views/404.vue';
 
 const routes = [
 	{
 		path: '/',
 		name: 'Home',
 		component: Home
+	},
+	{
+		path: '/login',
+		redirect: '/'
+	},
+
+	{
+		path: '/:pathMatch(.*)*',
+		name: 'NotFound',
+		component: NotFound
 	}
 
 	// Add more routes as needed
@@ -16,6 +26,15 @@ const routes = [
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes,
+});
+
+router.beforeEach((to, from, next) => {
+	const permanentRedirects = ['/login'];
+	if (permanentRedirects.includes(to.path)) {
+		window.location.replace('/');
+		return;
+	}
+	next();
 });
 
 export default router;
